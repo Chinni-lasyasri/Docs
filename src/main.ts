753503798affe6cb2530fe73f5afc0f85e0c5f4b
lasya-railtelit <maddipatisrilasya@gmail.com>
2026-03-25 16:19:27 +0530

    added cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,16 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    // origin: [
+    //   'https://docs-ui-azure.vercel.app',
+    //   'https://docs-8x8sfcp8m-chinni-lasyasris-projects.vercel.app',
+    // ],
+    origin: '*',
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
